5a6f176bfc6cc1fdbf97f90952bc05a116cac814
Nolan Keim <nolobot1995@gmail.com>
2021-06-12 15:43:41 -0400

    Converted example to read samples from esp32 sampling at around 40k and print them in a nice chart..

--- py_fft.py
+++ py_fft.py
@@ -9,50 +9,41 @@ import serial
 import time
 
 print("Hello")
-N = 1000
-Fs = 41466
+N = 10000
+Fs = 2777
+Ts = 1.0 / Fs
+print("N = {}".format(N))
+print("Ts = {}".format(Ts))
 
-ser = serial.Serial('/dev/ttyS3', baudrate=2000000) 
-esp32_data_raw = ser.read(N * 2) # 2 bytes per sample
-count = len(esp32_data_raw) // 2 # 2 bytes per sample
-print(type(esp32_data_raw))
+ser = serial.Serial('/dev/ttyS10', baudrate=2000000) 
+ser.write(b'r') # 2 bytes per sample
+esp32_data_packet = ser.readline().decode("utf-8")  # 2 bytes per sample
+esp32_data_raw_str = esp32_data_packet.split(',')
 
-esp32_data_int = struct.unpack('<' + count * 'H', esp32_data_raw)
-print(type(esp32_data_int))
+esp32_data_raw = [int(x) for x in esp32_data_raw_str]
+
+# esp32_data_int_lsb = struct.unpack('<' + 'H' * count, esp32_data_raw)
+# esp32_data_int_msb = struct.unpack('>' + 'H' * count, esp32_data_raw)
+# esp32_data_int = struct.unpack('B' * len(esp32_data_raw), esp32_data_raw)
 
 # reading in wave file with scipy
 # samplerate, samples = wavfile.read('CSC_sine_1000Hz.wav')
-samples = np.asarray(esp32_data_int) 
-# scalar = (2 ^ 5)
-# samples = np.true_divide(samples, scalar)
-y = samples[0:N]
+# samples_lsb = np.asarray(esp32_data_int_lsb) 
+# samples_msb = np.asarray(esp32_data_int_msb) 
 
-print (y.size)
-T = 1.0 / Fs
-print("N = {}".format(N))
-print("T = {}".format(T))
-
-x = np.linspace(0.0, N*T, N, endpoint=False)
-# y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
-
-xf = fftfreq(N, T)[:N//2]
-yf = fft(y)
-
-
-print(yf[10])
+# y_lsb = samples_lsb[0:N]
+# y_msb = samples_msb[0:N]
+x = np.linspace(0.0, N*Ts, N, endpoint=False)
 
 # plot time domain
-
-plt.plot(x, y) 
+plt.plot(esp32_data_raw) 
 plt.show()
-# time.sleep(10)
-
-
 
 # plot frequency domain
+# xf = fftfreq(N, Ts)[:N//2]
+# yf = fft(y)
 # plt.plot(xf, 2.0/N * np.abs(yf[0:N//2])) 
 # x_index_max = y.size / 2
 # plt.xlim([0, x_index_max])
-
 
 ser.close()             # close port
